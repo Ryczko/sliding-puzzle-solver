9f57cfbd1b49ee056db6b49e6a1a5bc8c59652be
Ryczko <konradryczko1@gmail.com>
2021-11-14 13:03:21 +0100

    Add image generating

--- src/components/Puzzle.ts
+++ src/components/Puzzle.ts
@@ -12,9 +12,15 @@ export class Puzzle {
         this.puzzle.style.width = this.size + 'px';
         this.puzzle.style.height = this.size + 'px';
         this.puzzle.textContent = this.value.toString();
+        this.puzzle.dataset.value = this.value.toString();
+
         this.puzzle.style.top = `${this.x * this.size}px`;
         this.puzzle.style.left = `${this.y * this.size}px`;
         this.puzzle.classList.add('puzzle');
+
+        this.puzzle.style.backgroundImage = `url(${this.image})`;
+        this.puzzle.style.backgroundPositionX = `-${this.y * this.size}px`;
+        this.puzzle.style.backgroundPositionY = `-${this.x * this.size}px`;
 
         this.updatePosition({ x: this.x, y: this.y });
     }
